cd572638d6796406163760fd3479189a4a07d7d1
kai202303 <129347351+kai202303@users.noreply.github.com>
2023-04-04 11:03:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ import altair
 st.title('飲食店データ webあぷり')
 st.caption('Powered by k')
 st.subheader('使い方 ガイド')
-st.text('外食産業のデータの利活用を目的とした、WEBアプリ開発の試作品')
+st.text('外食産業のデータの利活用を目的とした、WEBアプリ開発の試作品です。')
+st.text('サイドバーに氏名などを登録してデータにアクセスしてください。')
 image = Image.open('GPT.png')
 st.sidebar.image(image,width=300)
 
@@ -25,6 +26,9 @@ with st.sidebar.form(key='profile_from'):
     company = st.selectbox(
         '会社名',
         ('株式会社','有限会社','合同会社'))
+    Department = st.selectbox(
+            '部署名（任意項目）*',
+            ('本社営業部','大阪支店','名古屋支店','横浜支店','TCPリーシング課'))     
     name = st.text_input('氏名 *')
     Department = st.selectbox(
         '部署名(任意項目)',
@@ -32,10 +36,16 @@ with st.sidebar.form(key='profile_from'):
     access = st.radio(
         'アクセス先 *',
         ('会社','在宅'))
+            # 複数選択
+    Usage = st.multiselect(
+            '用途 (複数選択可) *',
+            ('営業資料','市場調査','データ分析','その他')
     resoiution = st.slider('chatGPT 解像度',0,100,50)
         # ボタン
     submit_btn = st.form_submit_button('送信')
-    cancel_btn = st.form_submit_button('キャンセル')
+    cancel_btn = st.form_submit_button('キャンセル') 
+if submit_btn:
+    st.text(f'{name}さん、いらっしゃいませ！')
     
 if submit_btn:
     df = pd.read_csv('tabe.csv', encoding='cp932')
